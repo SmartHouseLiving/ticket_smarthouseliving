5346a43d6f596825ce62cf54fedaf5c7726b907c
Silvaa27 <rodrigobesilva875@gmail.com>
2026-03-11 15:58:45 +0000

    add welcome mensage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="min-h-0 flex-1">
+    <div class="min-h-0 flex-1 bg-slate-50">
 
         @include('partials.header')
 
-        <livewire:creators-ticketing.ticket-submit-form />
+        <div class="mx-auto max-w-7xl px-6 py-8 sm:px-8 lg:px-10">
+            <livewire:creators-ticketing.ticket-submit-form />
+        </div>
+
     </div>
 @endsection
